33e395fbcd3aa1910a7039e0d16b90ab7dd7ff4a
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-16 16:45:27 -0600

    Start working on fetching jokes from API

---
+++ dad-jokes-app/src/Joke.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+
+
+
+class Joke  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+        
+    }
+
+    async componentDidMount() {
+      
+    }
+  
+    render() {
+        
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Joke;
---
+++ dad-jokes-app/src/JokeList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Joke from "./Joke";
+
+
+
+class JokeList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { };
+        
+    }
+
+    async componentDidMount() {
+      
+    }
+  
+    render() {
+        
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default JokeList;
