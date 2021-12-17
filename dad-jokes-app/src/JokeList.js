573e8cb0ca090d251580ba97aec5a2f853bc097d
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-16 18:01:56 -0600

    Card fetching is done

--- dad-jokes-app/src/JokeList.js
+++ dad-jokes-app/src/JokeList.js
@@ -2,24 +2,35 @@ import React, { Component } from "react";
 import axios from "axios";
 import Joke from "./Joke";
 
-
-
 class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10,
+    };
+
     constructor(props) {
         super(props);
-        this.state = { };
-        
+        this.state = { jokes: [] };
     }
 
     async componentDidMount() {
-      
+        let jokes = [];
+        while (jokes.length < this.props.numJokesToGet) {
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { Accept: "application/json" },
+            });
+            jokes.push(res.data.joke);
+        }
+        this.setState({ jokes: jokes });
     }
-  
+
     render() {
-        
         return (
-            <div>
-                
+            <div className="JokeLists">
+                <h1>Dad's jokes</h1>
+                {this.state.jokes.map((joke) => (
+                    <div> {joke}</div>
+                ))}
+                ;
             </div>
         );
     }
