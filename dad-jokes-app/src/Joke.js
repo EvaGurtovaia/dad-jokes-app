022dd50ea328ddaf7585d3c34a1e26de441dc510
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-20 15:43:12 -0600

    Working on adding dynamic colors and emojies

--- dad-jokes-app/src/Joke.js
+++ dad-jokes-app/src/Joke.js
@@ -7,6 +7,23 @@ class Joke extends Component {
         super(props);
         this.state = {};
     }
+    getBorderColor() {
+        if (this.props.votes >= 15) {
+            return "#4CAF50";
+        } else if (this.props.votes >= 12) {
+            return "#8BC34A";
+        } else if (this.props.votes >= 9) {
+            return "#CDDC39";
+        } else if (this.props.votes >= 6) {
+            return "#FFEB3B";
+        } else if (this.props.votes >= 3) {
+            return "#FFC107";
+        } else if (this.props.votes >= 0) {
+            return "#FF9800";
+        } else {
+            return "#f44336";
+        }
+    }
 
     async componentDidMount() {}
 
@@ -18,7 +35,12 @@ class Joke extends Component {
                         className="fas fa-arrow-up"
                         onClick={this.props.upvote}
                     />
-                    <span className="Joke-votes">{this.props.votes}</span>
+                    <span
+                        className="Joke-votes"
+                        style={{ borderColor: this.getBorderColor() }}
+                    >
+                        {this.props.votes}
+                    </span>
                     <i
                         className="fas fa-arrow-down"
                         onClick={this.props.downvote}
@@ -26,7 +48,11 @@ class Joke extends Component {
                 </div>
                 <div className="Joke-text">{this.props.text}</div>
                 <div className="Joke-smiley">
-                    <i />
+                    <i
+                        class="em em-smiley_cat"
+                        aria-role="presentation"
+                        aria-label="SMILING CAT FACE WITH OPEN MOUTH"
+                    ></i>
                 </div>
             </div>
         );
